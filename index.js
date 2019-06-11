a79cb1291e5cbc111779d4080c1e84f2ea8be058
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-11 11:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-  parseInt(n)
-  // Feel free to move things around!
   const two = 2
+  return n + two
+  // Feel free to move things around!
 }
 
 
